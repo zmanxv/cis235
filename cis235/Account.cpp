c6936d3a35706c1c29c4ee5e7ee823e505bb21e0
hecarp <hhimes8499@students.mc3.edu>
2016-04-25 20:00:45 -0400

    Update Account.cpp
    Moved validator functions to cpp file

--- cis235/Account.cpp
+++ cis235/Account.cpp
@@ -4,6 +4,46 @@
 using namespace std;
 
 //***********************************************************************
+// The validateDay function validates that the day is valid.		*
+// Parameters: None.							*
+//***********************************************************************
+void Account::validateDay()
+	{
+		if (day < MIN_DATE || day > MAX_DATE)
+		{
+			throw InvalidDay();
+		}
+		// Test for leap year
+		else if (month == 2)
+		{
+			if (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) && day > 29)
+			{
+				throw InvalidDay();	
+			}
+			else if (day > 28)
+			{
+				throw InvalidDay();
+			}
+		}
+		else if ((month == 4 || month == 6 || month == 9, || month = 11) && day > (MAX_DATE-1))
+		{
+			throw InvalidDay();
+		}
+	}
+
+//***********************************************************************
+// The validateMonth function validates that the day is valid.		*
+// Parameters: None.							*
+//***********************************************************************
+void Account::validateMonth()
+	{
+		if (month < MIN_MONTH || month > MAX_MONTH)
+		{
+			throw InvalidMonth();
+		}
+	}
+
+//***********************************************************************
 // The deposit function adds a Transaction to the Account balance and	*
 // increases the transaction count.										*
 // Parameters: A Transaction, which includes the amount to be deposited.*
