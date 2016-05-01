557807cedf4b03adcddd9e0adbf56a564797457e
Matt <zmanxvi@gmail.com>
2016-05-01 15:11:36 -0400

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -27,6 +27,7 @@ void menu()
 	while(runMenu)
 	{
 		cout << "[A]dd an account" << endl;
+		cout << "[I]nput a new transaction to an account" << endl;
 		cout << "[V]iew transaction history" << endl;
 		cout << "[S]earch for a transaction" << endl;
 		cout << "[G]et a summary of all accounts" << endl;
@@ -40,111 +41,130 @@ void menu()
 			if (confirm == 'c' || confirm == 'C')
 			{
 				//add checking account
-				cout << "Do you wish to enter debit or credit?" << endl;
-				cin >> confirm;
-				if (confirm == 'c' || confirm == 'C')
+				while(confirm != 'n' || confirm != 'N')
 				{
-					//credit
-					cout << "Enter Name of transaction" << endl;
-					cin >> name;
+					cout << "Do you wish to enter debit or credit?" << endl;
+					cin >> confirm;
+					if (confirm == 'c' || confirm == 'C')
+					{
+						//credit
+						cout << "Enter Name of transaction" << endl;
+						cin >> name;
 
-					cout << "Date of transaction" << endl;
-					cin >> date;
+						cout << "Date of transaction" << endl;
+						cin >> date;
 
-					cout << "Enter description" << endl;
-					cin >> description;
+						cout << "Enter description" << endl;
+						cin >> description;
 
-					cout << "Enter the amount of the transaction" << endl;
-					cin >> amount;
-				}
-				if (confirm == 'd' || confirm == 'D')
-				{
-					//debit
-					cout << "Enter Name of transaction" << endl;
-					cin >> name;
+						cout << "Enter the amount of the transaction" << endl;
+						cin >> amount;
+					}
+					if (confirm == 'd' || confirm == 'D')
+					{
+						//debit
+						cout << "Enter Name of transaction" << endl;
+						cin >> name;
 
-					cout << "Date of transaction" << endl;
-					cin >> date;
+						cout << "Date of transaction" << endl;
+						cin >> date;
 
-					cout << "Enter description" << endl;
-					cin >> description;
+						cout << "Enter description" << endl;
+						cin >> description;
 
-					cout << "Enter the amount of the transaction" << endl;
-					cin >> amount;
+						cout << "Enter the amount of the transaction" << endl;
+						cin >> amount;
+					}
+					cout << "Do you wish to enter another transaction?" << endl;
+					cin >> confirm;
 				}
 			}
 			if (confirm == 's' || confirm == 'S')
 			{
 				//add saving account
-				cout << "Do you wish to enter debit or credit?" << endl;
-				cin >> confirm;
-				if (confirm == 'c' || confirm == 'C')
+			while(confirm != 'n' || confirm != 'N')
 				{
-					//credit
-					cout << "Enter Name of transaction" << endl;
-					cin >> name;
+					cout << "Do you wish to enter debit or credit?" << endl;
+					cin >> confirm;
+					if (confirm == 'c' || confirm == 'C')
+					{
+						//credit
+						cout << "Enter Name of transaction" << endl;
+						cin >> name;
 
-					cout << "Date of transaction" << endl;
-					cin >> date;
+						cout << "Date of transaction" << endl;
+						cin >> date;
 
-					cout << "Enter description" << endl;
-					cin >> description;
+						cout << "Enter description" << endl;
+						cin >> description;
 
-					cout << "Enter the amount of the transaction" << endl;
-					cin >> amount;
-				}
-				if (confirm == 'd' || confirm == 'D')
-				{
-					//debit
-					cout << "Enter Name of transaction" << endl;
-					cin >> name;
+						cout << "Enter the amount of the transaction" << endl;
+						cin >> amount;
+					}
+					if (confirm == 'd' || confirm == 'D')
+					{
+						//debit
+						cout << "Enter Name of transaction" << endl;
+						cin >> name;
 
-					cout << "Date of transaction" << endl;
-					cin >> date;
+						cout << "Date of transaction" << endl;
+						cin >> date;
 
-					cout << "Enter description" << endl;
-					cin >> description;
+						cout << "Enter description" << endl;
+						cin >> description;
 
-					cout << "Enter the amount of the transaction" << endl;
-					cin >> amount;
+						cout << "Enter the amount of the transaction" << endl;
+						cin >> amount;
+					}
+					cout << "Do you wish to enter another transaction?" << endl;
+					cin >> confirm;
 				}
 			}
 			if (confirm == 'm' || confirm == 'M')
 			{
 				//add Money Market account
-				cout << "Do you wish to enter debit or credit?" << endl;
-				cin >> confirm;
-				if (confirm == 'c' || confirm == 'C')
+				while(confirm != 'n' || confirm != 'N')
 				{
-					//credit
-					cout << "Enter Name of transaction" << endl;
-					cin >> name;
+					cout << "Do you wish to enter debit or credit?" << endl;
+					cin >> confirm;
+					if (confirm == 'c' || confirm == 'C')
+					{
+						//credit
+						cout << "Enter Name of transaction" << endl;
+						cin >> name;
 
-					cout << "Date of transaction" << endl;
-					cin >> date;
+						cout << "Date of transaction" << endl;
+						cin >> date;
 
-					cout << "Enter description" << endl;
-					cin >> description;
+						cout << "Enter description" << endl;
+						cin >> description;
 
-					cout << "Enter the amount of the transaction" << endl;
-					cin >> amount;
-				}
-				if (confirm == 'd' || confirm == 'D')
-				{
-					//debit
-					cout << "Enter Name of transaction" << endl;
-					cin >> name;
+						cout << "Enter the amount of the transaction" << endl;
+						cin >> amount;
+					}
+					if (confirm == 'd' || confirm == 'D')
+					{
+						//debit
+						cout << "Enter Name of transaction" << endl;
+						cin >> name;
 
-					cout << "Date of transaction" << endl;
-					cin >> date;
+						cout << "Date of transaction" << endl;
+						cin >> date;
 
-					cout << "Enter description" << endl;
-					cin >> description;
+						cout << "Enter description" << endl;
+						cin >> description;
 
-					cout << "Enter the amount of the transaction" << endl;
-					cin >> amount;
+						cout << "Enter the amount of the transaction" << endl;
+						cin >> amount;
+					}
+					cout << "Do you wish to enter another transaction?" << endl;
+					cin >> confirm;
 				}
 			}
+		}
+		if (confirm == 'i' || confirm == 'I')
+		{
+			//add a new transaction to an existing account
 		}
 		if (confirm == 'v' || confirm == 'V')
 		{
